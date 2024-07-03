f408d5a66eb90b80db4ad86dacbeae1364e1936e
alatjoseph <alatjoseph625@gmail.com>
2024-07-03 14:40:19 +0530

    Login page completed

---
+++ LoginPage/loginpage/lib/loginpage.dart
@@ -0,0 +1,153 @@
+import 'package:flutter/material.dart';
+
+class Loginpage extends StatefulWidget {
+  const Loginpage({super.key});
+
+  @override
+  State<Loginpage> createState() => _LoginpageState();
+}
+
+bool checkboxValue = false;
+
+class _LoginpageState extends State<Loginpage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+          child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 30),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Text(
+              'Welcome back',
+              style: TextStyle(fontSize: 20),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            const Text(
+              'Login to your account',
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            TextFormField(
+              decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: 'Email ',
+                  hintStyle: TextStyle(fontSize: 20, color: Colors.black87)),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            TextFormField(
+              decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: 'Password ',
+                  hintStyle: TextStyle(fontSize: 20, color: Colors.black87)),
+            ),
+            Row(
+              children: [
+                Checkbox(
+                  checkColor: Colors.white,
+                  activeColor: Colors.green,
+                  value: checkboxValue,
+                  onChanged: (bool? value) {
+                    setState(() {
+                      checkboxValue = !checkboxValue;
+                    });
+                  },
+                ),
+                const Text(
+                  'Remember me',
+                  style: TextStyle(
+                      fontSize: 16,
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(
+                  width: 40,
+                ),
+                TextButton(
+                    onPressed: () {},
+                    child: const Text('Forgot password?',
+                        style: TextStyle(
+                            fontSize: 16,
+                            color: Colors.blue,
+                            fontWeight: FontWeight.bold)))
+              ],
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Center(
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(7),
+                  color: Colors.green,
+                ),
+                width: MediaQuery.of(context).size.width * .86,
+                height: 55,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.green,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(5)),
+                  ),
+                  onPressed: () {},
+                  child: const Text(
+                    'Login now',
+                    style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Center(
+              child: Container(
+                width: MediaQuery.of(context).size.width * .86,
+                height: 60,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(7),
+                  color: Colors.black,
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                
+                 Container(
+                  height: 30,
+                  width: 30,
+                  child: const Image(image:NetworkImage('https://logopng.com.br/logos/google-37.png'))),
+                
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.black,
+                      ),
+                      onPressed: () {},
+                      child: const Text(
+                        'Continue with Google',
+                        style: TextStyle(
+                            fontSize: 20,
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      )),
+    );
+  }
+}
